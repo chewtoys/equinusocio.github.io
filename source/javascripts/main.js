f796a1d0995499b90a357a311dd9a06427ab9188
Mattia Astorino <astorino.mattia@gmail.com>
2017-08-05 21:59:26 +0200

    chore: Add background mouse position

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -6,6 +6,19 @@ window.onload = function () {
   particlesJS.load('DustJs', '/javascripts/particles.json', function () {
     console.log('callback - particles.js config loaded');
   });
+
+  var movementStrength = 55;
+  var height = movementStrength / window.innerWidth;
+  var width = movementStrength / window.innerWidth;
+
+  document.addEventListener('mousemove', function (event) {
+    var pageX = event.pageX - (window.innerWidth / 2);
+    var pageY = event.pageY - (window.innerHeight / 2);
+    var newvalueX = width * pageX * -1 - 25;
+    var newvalueY = height * pageY * -1 - 50;
+    var htmlElement = document.querySelector('.MoveBackground');
+    htmlElement.style.backgroundPosition = newvalueX + 'px     ' + newvalueY + 'px';
+  });
 }
 
 
