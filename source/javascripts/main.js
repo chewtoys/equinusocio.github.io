81a23fcf5be92b8b9307f91c05d3f4afee6d1c22
Mattia Astorino <astorino.mattia@gmail.com>
2017-08-05 19:43:25 +0200

    feat: Add particle js

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,4 +2,10 @@ window.onload = function () {
   var audio = document.getElementById("bgsound");
   audio.volume = 0.4;
   audio.play();
+
+  particlesJS.load('DustJs', '/javascripts/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+  });
 }
+
+
