b2f0d7bb7a4ccec9c03fa08e6f8598a8292874ea
Mattia Astorino <astorino.mattia@gmail.com>
2017-08-17 14:03:18 +0200

    chore: Add link hover audio

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,11 +1,32 @@
 window.onload = function () {
-  var audio = document.getElementById("bgsound");
-  audio.volume = 0.4;
-  audio.play();
+  var bgSound = document.getElementById("bgsound");
+  var hoverSound = new Audio("../Expand.m4a");
+  var outSound = new Audio("../Collapse.m4a");
 
-  particlesJS.load('DustJs', '/javascripts/particles.json', function () {
-    console.log('callback - particles.js config loaded');
-  });
+  hoverSound.volume = 0.4;
+  outSound.volume = 0.4;
+  bgSound.volume = 0.4;
+  bgSound.play();
+
+  Array.prototype.forEach.call(
+    document.querySelectorAll('.c-displayLink'), function (link) {
+      link.addEventListener('mouseover', function(){
+        outSound.pause();
+        outSound.currentTime = 0;
+        hoverSound.pause();
+        hoverSound.currentTime = 0;
+        hoverSound.play();
+      });
+      link.addEventListener('mouseout', function(){
+        outSound.pause();
+        outSound.currentTime = 0;
+        outSound.play();
+      });
+    }
+  )
+
+  particlesJS.load('DustJs', '/javascripts/particles.json');
+
 
   var movementStrength = 55;
   var height = movementStrength / window.innerWidth;
