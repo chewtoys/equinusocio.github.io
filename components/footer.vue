d9d7872e2c69057335935996ba33a9d675346f27
Mattia Astorino <astorino.mattia@gmail.com>
2018-10-17 12:33:40 +0200

    chore: Fix footer margins on mobile

--- components/footer.vue
+++ components/footer.vue
@@ -5,13 +5,21 @@
 </template>
 
 <style lang="postcss" scoped>
+@import '../assets/media.pcss';
+
 .Footer {
   font-size: 14px;
   margin: 0 auto;
   text-align: center;
   color: var(--foregroundColor, #000);
   margin-top: 216px;
-  margin-bottom: 16px;
+  margin-bottom: 100px;
+}
+
+@media (--medium) {
+  .Footer {
+    margin-bottom: 16px;
+  }
 }
 </style>
 
