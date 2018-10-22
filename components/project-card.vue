06cfd12972c896dc274fcddea0dcbd058c070bdb
Mattia Astorino <astorino.mattia@gmail.com>
2018-10-22 17:55:12 +0200

    chore: Add project wip

---
+++ components/project-card.vue
@@ -0,0 +1,67 @@
+<template>
+  <article class="ProjectCard">
+    <figure>
+      <img class="ProjectImage" :src="image" :alt="title" draggable="false">
+    </figure>
+    <a :href="url" :title="title" class="ProjectTitle">
+      <span class="ProjectCounter">{{counter}}</span>
+      <h1 class="SubTitle">{{title}}</h1>
+    </a>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: {
+      url: {
+        type: String,
+        default: '#'
+      },
+      title: {
+        type: String,
+        default: '',
+        required: true
+      },
+      image: {
+        type: String,
+        required: true
+      },
+      counter: {
+        type: String,
+        default: '01',
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="postcss" scoped>
+.ProjectCard {
+  position: relative;
+}
+
+
+.ProjectTitle {
+  text-decoration: none;
+  display: grid;
+  grid-template-columns: minmax(40px, 60px) auto;
+  grid-gap: 24px;
+  padding: 24px 0;
+
+  &:hover {
+    color: var(--callToActionColor, #00E2BC);
+  }
+
+  & > * {
+    font-weight: inherit;
+    margin: 0;
+  }
+}
+
+.ProjectCounter {
+  font-weight: 900;
+  font-size: 40px;
+  color: var(--callToActionColor, #00E2BC);
+}
+</style>
+
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,60 @@
 <template>
   <main role="main">
     <Header />
+    <HeroBanner height="calc(100vh - 200px)">
+      <h1 class="DisplayTitle">My personal laboratory</h1>
+    </HeroBanner>
 
-    <h1>Projects</h1>
+    <section class="ProjectList">
+      <ProjectCard
+        url="/projects/material-theme"
+        image="/prjscovers/vscmt.svg"
+        title="Material Theme for Visual Studio Code"
+        counter="01">
+      </ProjectCard>
+
+      <ProjectCard
+        url="/projects/gaming-photobook"
+        image="/prjscovers/gamingpb.svg"
+        title="Gaming photobook from my PS4"
+        counter="02">
+      </ProjectCard>
+
+      <ProjectCard
+        image="/prjscovers/blob.svg"
+        title="Animated CSS blob element"
+        counter="03">
+      </ProjectCard>
+    </section>
+
+    <Footer />
   </main>
 </template>
 
 <script>
 import Header from '~/components/header.vue'
+import HeroBanner from '~/components/herobanner.vue'
+import ProjectCard from '~/components/project-card.vue'
+import Footer from '~/components/footer.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    HeroBanner,
+    ProjectCard,
+    Footer
   }
 }
 </script>
+
+
+<style lang="postcss" scoped>
+.ProjectList {
+  display: grid;
+  max-width: 900px;
+  padding: 0 32px;
+  margin: auto;
+  grid-gap: 24px;
+  grid-template-columns: repeat(auto-fill, minmax(280px, auto));
+}
+</style>
