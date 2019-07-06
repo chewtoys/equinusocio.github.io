4db8390ba580773bcf713ac56c6b9010169171f8
Mattia Astorino <astorino.mattia@gmail.com>
2019-07-06 11:49:37 +0200

    Chore add now and fix icons fills

--- components/article/article-preview.vue
+++ components/article/article-preview.vue
@@ -1,107 +1,141 @@
 <template>
-  <article :class="`ArticlePreview ${direction}`" itemscope itemtype="http://schema.org/NewsArticle" author="Mattia Astorino">
+  <article
+    :class="`ArticlePreview ${direction}`"
+    itemscope
+    itemtype="http://schema.org/NewsArticle"
+    author="Mattia Astorino"
+  >
     <lazy-component @show="onScreen" class="ArticleImage">
       <figure v-if="visible">
-        <picture v-if="post.fields.heroImage.fields.file" itemprop="image" itemscope itemtype="http://schema.org/ImageObject">
+        <picture
+          v-if="post.fields.heroImage.fields.file"
+          itemprop="image"
+          itemscope
+          itemtype="http://schema.org/ImageObject"
+        >
           <source
             type="image/webp"
             :srcset="`https:${post.fields.heroImage.fields.file.url}?w=410&h=467&fm=webp, https:${post.fields.heroImage.fields.file.url}?fm=webp 2x`"
             draggable="false"
             :alt="post.fields.title"
-          >
+          />
           <source
             type="image/png"
             :srcset="`https:${post.fields.heroImage.fields.file.url}?w=410&h=467&fm=png, https:${post.fields.heroImage.fields.file.url}?fm=jpg 2x`"
             draggable="false"
             :alt="post.fields.title"
-          >
+          />
           <img
             :srcset="`https:${post.fields.heroImage.fields.file.url}?w=410&h=467&fm=jpg, https:${post.fields.heroImage.fields.file.url}?fm=jpg 2x`"
             draggable="false"
             :alt="post.fields.title"
             :src="`https:${post.fields.heroImage.fields.file.url}?w=410&h=467&fm=webp`"
-          >
+          />
         </picture>
       </figure>
     </lazy-component>
 
-    <div class="ArticleContent" >
-
+    <div class="ArticleContent">
       <header>
         <aside class="MetaContainer">
-          <Datetime itemprop="datePublished" :content="shortDate" :date="new Date(post.fields.publishDate)" />
+          <Datetime
+            itemprop="datePublished"
+            :content="shortDate"
+            :date="new Date(post.fields.publishDate)"
+          />
           <TagList class="TagsContainer">
             <Tag :tag="tag" :key="tag" v-for="tag in post.fields.tags">{{tag}}</Tag>
           </TagList>
         </aside>
 
         <h1 class="PostTitle" itemprop="headline">
-          <a rel="noopener" v-if="post.fields.externalUrl" target="_blank" :href="post.fields.externalUrl">
-            {{ post.fields.title }}
-          </a>
-          <nuxt-link v-else :to="{ name: 'blog-slug', params: { slug: post.fields.slug, id: post.sys.id }}">
-            {{ post.fields.title }}
-          </nuxt-link>
+          <a
+            rel="noopener"
+            v-if="post.fields.externalUrl"
+            target="_blank"
+            :href="post.fields.externalUrl"
+          >{{ post.fields.title }}</a>
+          <nuxt-link
+            v-else
+            :to="{ name: 'blog-slug', params: { slug: post.fields.slug, id: post.sys.id }}"
+          >{{ post.fields.title }}</nuxt-link>
         </h1>
       </header>
 
       <p class="SmallBody" itemprop="description">{{ post.fields.description }}</p>
 
-      <a rel="noopener" v-if="post.fields.externalUrl" ne-button target="_blank" :href="post.fields.externalUrl">
+      <a
+        rel="noopener"
+        v-if="post.fields.externalUrl"
+        ne-button
+        target="_blank"
+        :href="post.fields.externalUrl"
+      >
         Read this story
-        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M12 5V11C12 11.6 11.6 12 11 12C10.4 12 10 11.6 10 11V7.4L5.7 11.7C5.5 11.9 5.3 12 5 12C4.7 12 4.5 11.9 4.3 11.7C3.9 11.3 3.9 10.7 4.3 10.3L8.6 6H5C4.4 6 4 5.6 4 5C4 4.4 4.4 4 5 4H11C11.6 4 12 4.4 12 5Z"/>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            fill="currentColor"
+            d="M12 5V11C12 11.6 11.6 12 11 12C10.4 12 10 11.6 10 11V7.4L5.7 11.7C5.5 11.9 5.3 12 5 12C4.7 12 4.5 11.9 4.3 11.7C3.9 11.3 3.9 10.7 4.3 10.3L8.6 6H5C4.4 6 4 5.6 4 5C4 4.4 4.4 4 5 4H11C11.6 4 12 4.4 12 5Z"
+          />
         </svg>
-
       </a>
-      <nuxt-link v-else tag="button" ne-button :to="{ name: 'blog-slug', params: { slug: post.fields.slug, id: post.sys.id }}">
-        Read this story
-      </nuxt-link>
-
+      <nuxt-link
+        v-else
+        tag="button"
+        ne-button
+        :to="{ name: 'blog-slug', params: { slug: post.fields.slug, id: post.sys.id }}"
+      >Read this story</nuxt-link>
     </div>
   </article>
 </template>
 
 <script>
-  import FormatDate from '~/plugins/formatDate'
+import FormatDate from '~/plugins/formatDate'
 
-  export default {
-    props: {
-      post: Object,
-      index: Number
-    },
-    data() {
-      return {
-        visible: false
+export default {
+  props: {
+    post: Object,
+    index: Number
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  computed: {
+    direction() {
+      if (this.index % 2 === 0) {
+        return ''
+      } else {
+        return 'Alt'
       }
     },
-    computed: {
-      direction() {
-        if (this.index % 2 === 0) {
-          return ''
-        } else {
-          return 'Alt'
-        }
-      },
-      shortDate() {
-        return FormatDate(this.post.fields.publishDate, {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit'
-        })
-      }
-    },
-    components: {
-      Tag: () => import('~/components/article/tag.vue'),
-      TagList: () => import('~/components/article/taglist.vue'),
-      Datetime: () => import('~/components/article/datetime.vue')
-    },
-    methods: {
-      onScreen() {
-        this.visible = true
-      }
+    shortDate() {
+      return FormatDate(this.post.fields.publishDate, {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      })
+    }
+  },
+  components: {
+    Tag: () => import('~/components/article/tag.vue'),
+    TagList: () => import('~/components/article/taglist.vue'),
+    Datetime: () => import('~/components/article/datetime.vue')
+  },
+  methods: {
+    onScreen() {
+      this.visible = true
     }
   }
+}
 </script>
 
 <style scoped lang="postcss">
--- static/sw.js
+++ static/sw.js
@@ -2,16 +2,8 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0987c30455afdadb3671.js",
-    "revision": "9cbe87012029769f0a0876d354fdaafd"
-  },
-  {
-    "url": "/_nuxt/0a06d88cb316faef5116.css",
-    "revision": "23498596a5cd89c79a76385cdb60c22c"
-  },
-  {
-    "url": "/_nuxt/0e1d6fffa26802104ed5.js",
-    "revision": "f333e06503196970c0ebac6b151f94ff"
+    "url": "/_nuxt/0c577d4b54f39d5a458a.js",
+    "revision": "3269627be3c8d8249b48e2edb19c0106"
   },
   {
     "url": "/_nuxt/15a4ac343b56bf802150.css",
@@ -30,10 +22,6 @@ workbox.precaching.precacheAndRoute([
     "revision": "09c6d42ea641aa425b8682b66ce9956a"
   },
   {
-    "url": "/_nuxt/23c0626a464b16d5d703.js",
-    "revision": "5622eacb63f4e2ee9f16568220bd0f37"
-  },
-  {
     "url": "/_nuxt/243eb061abadbbb96574.css",
     "revision": "fd4dff56616fa52ae6ab000aac7e76a5"
   },
@@ -50,6 +38,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "dbf8652397fb908679e86bd42114c3d7"
   },
   {
+    "url": "/_nuxt/3b913339785c26764d1f.css",
+    "revision": "e0a3a85a0ec4a6c39f332c44a8b44ae1"
+  },
+  {
     "url": "/_nuxt/3db2e0de6b34a1d406b0.css",
     "revision": "d2b652dee2abc6b096e92a56bdd7e0af"
   },
@@ -62,20 +54,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "0abcc959f3e073767219d7066a07d3c1"
   },
   {
+    "url": "/_nuxt/4c56333d249b6c41c74e.css",
+    "revision": "eb2b5c2c353bd1ce953f35a455801b67"
+  },
+  {
     "url": "/_nuxt/59d5dc2b920b0aea8891.js",
     "revision": "bf67b560142ea9af1caecb5d56187abb"
+  },
+  {
+    "url": "/_nuxt/5a935eba754d3614410b.css",
+    "revision": "2ab72a7f5e0d41c0ca25b60a360227d0"
   },
   {
     "url": "/_nuxt/5e94519797508b1923fe.css",
     "revision": "ccc2ddbfd3c36af1f4f74d49ba027fc9"
   },
   {
-    "url": "/_nuxt/66bd11ad0e0839fe8c80.css",
-    "revision": "13853926572bc609687cc1e3dc614909"
-  },
-  {
-    "url": "/_nuxt/7b87b7ce36476ea549fa.css",
-    "revision": "afd20f01e150984f4b3cf4530424c4e3"
+    "url": "/_nuxt/6db4aa99e8156ae6578b.js",
+    "revision": "8cb77427aeb835b446740e7cd3ad8cc7"
   },
   {
     "url": "/_nuxt/809faedaf166a8b54779.js",
@@ -90,12 +86,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "fd4cfb5491b11859957c5770b561e2f6"
   },
   {
-    "url": "/_nuxt/9212b97c651476f095d0.js",
-    "revision": "913040818607bd1c06e32f7de22f2b2a"
+    "url": "/_nuxt/89ecffc193085c426794.js",
+    "revision": "f4fc453e9f71fd96210eb55d1869a846"
+  },
+  {
+    "url": "/_nuxt/8d6e22f63398bd592d8b.js",
+    "revision": "dac57a4a2c24649c57f5abcdcdb64b79"
+  },
+  {
+    "url": "/_nuxt/916c7aae6b01662f4178.js",
+    "revision": "2c24e67a3662e1ab08c64bacd6c69820"
   },
   {
     "url": "/_nuxt/9421aebd7813e781dd08.js",
     "revision": "5cc72074174df6045921a868be8380a8"
+  },
+  {
+    "url": "/_nuxt/989a1d7f40ccba3288ae.js",
+    "revision": "41068d93db749a6fb7336db4464849d9"
   },
   {
     "url": "/_nuxt/9a62fa7b028d5a9dbb14.css",
@@ -106,32 +114,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "7ea804f1a31427a83926ffc7d1fa1156"
   },
   {
-    "url": "/_nuxt/9b4ae4bbbfd5d4669fa5.css",
-    "revision": "16418de864bf8146c33bc5c49f5e62e7"
-  },
-  {
-    "url": "/_nuxt/9ed2b637c0801eae70fe.js",
-    "revision": "4629c9f6da5be03facbbef8631eb0b1b"
-  },
-  {
-    "url": "/_nuxt/a8a4e9ddba09bd9dded5.js",
-    "revision": "c3f8a0f81193ab788fa74bea0f764494"
-  },
-  {
-    "url": "/_nuxt/b96eb75b8291d565c19d.css",
-    "revision": "211c5dd1ece0960e3a97252515800627"
+    "url": "/_nuxt/b36cf0b058b44475b3b5.css",
+    "revision": "6726be6319d1c53373d2828ddf180121"
   },
   {
     "url": "/_nuxt/bdd1974b7260aa92f94a.js",
     "revision": "9d8a33524a905cea6032b751b0eb8294"
   },
   {
-    "url": "/_nuxt/c94bfa40ad346fe38c5f.js",
-    "revision": "27176b5b409129f7da11d4f6357940c6"
+    "url": "/_nuxt/c23f75d40d0e7179b523.css",
+    "revision": "704a5100338e971dbe7899eb35a4cea0"
   },
   {
-    "url": "/_nuxt/cc64e3b0a20f287bd610.js",
-    "revision": "c504ee25c1005b6d3ecb8f95f911debb"
+    "url": "/_nuxt/c27d51abb88b0d96db8c.js",
+    "revision": "e07320f9b1b239af6acc3d30b47dc155"
+  },
+  {
+    "url": "/_nuxt/cadc3b2486dfa4ddafae.js",
+    "revision": "e8c2d41a49a9202fa98e0ff156e50137"
   },
   {
     "url": "/_nuxt/cd00a9879eccac68d851.css",
@@ -140,6 +140,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/d1a53be6ad21af7ea4fb.js",
     "revision": "8e6191220d428b382b39488eb40bdadb"
+  },
+  {
+    "url": "/_nuxt/d52b3b321704a55c1cb6.js",
+    "revision": "cb0d59bcd54d367385f1fc6ac8e58d88"
   },
   {
     "url": "/_nuxt/dd0425c15e03d16f5671.css",
@@ -162,10 +166,6 @@ workbox.precaching.precacheAndRoute([
     "revision": "ee528389ce217853c56b9ab8f9972da3"
   },
   {
-    "url": "/_nuxt/ec7b24f9a5d2b77b38c9.css",
-    "revision": "3e1f8bb9ed0538be839231e7c0ba56e1"
-  },
-  {
     "url": "/_nuxt/f05069eca979ad6d31a8.js",
     "revision": "d0a826cbc7c072a6755dda6b4ed76116"
   },
@@ -174,16 +174,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "d97cdabe50711094dd2896f2ac3167a9"
   },
   {
-    "url": "/_nuxt/f26625f56e01fcf53808.js",
-    "revision": "346881c28710cc82ac4fa6fad6ff1400"
-  },
-  {
     "url": "/_nuxt/f85bc58f3e527f755473.js",
     "revision": "92b6f2dd12a56b0e766cec4d9c0c926c"
   },
   {
     "url": "/_nuxt/f91fe0e0e82c24269020.js",
     "revision": "b190772e7e91199e74beaa1c61742bd9"
+  },
+  {
+    "url": "/_nuxt/f97d4711ca426fb5c218.css",
+    "revision": "90141d2f466c0391620762880f89dd1d"
   },
   {
     "url": "/_nuxt/f9e4f6e864343e906279.css",
