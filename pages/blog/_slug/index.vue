8d1d433c5f63cdcccb2314e88a8b621ac0fc9b98
Mattia Astorino <astorino.mattia@gmail.com>
2019-07-28 12:05:22 +0200

    Disable og images escaping

--- pages/blog/_slug/index.vue
+++ pages/blog/_slug/index.vue
@@ -8,7 +8,11 @@
         <h1 class="DisplayTitle" itemprop="headline">{{ post.fields.title }}</h1>
 
         <div class="PublishDate">
-          <Datetime itemprop="datePublished" :content="shortDate" :date="new Date(post.fields.publishDate)" />
+          <Datetime
+            itemprop="datePublished"
+            :content="shortDate"
+            :date="new Date(post.fields.publishDate)"
+          />
         </div>
         <ShareWidget class="Socials" :postTitle="post.fields.title" />
       </HeroBanner>
@@ -19,7 +23,6 @@
     </article>
 
     <Footer />
-
   </main>
 </template>
 
@@ -38,51 +41,108 @@ import createClient from '~/plugins/contentful.js'
 
 const client = createClient()
 
-
 export default {
-  asyncData ({ error, params }) {
-    return client.getEntries({
-      'content_type': 'blogPost',
-      'fields.slug': params.slug
-    }).then(entries => {
-      if (entries.items.length === 0) {
-        const status = { statusCode: 404, message: 'Post not found' }
-        throw (status)
-      } else {
-        return {
-          post: entries.items[0]
+  asyncData({ error, params }) {
+    return client
+      .getEntries({
+        content_type: 'blogPost',
+        'fields.slug': params.slug
+      })
+      .then(entries => {
+        if (entries.items.length === 0) {
+          const status = { statusCode: 404, message: 'Post not found' }
+          throw status
+        } else {
+          return {
+            post: entries.items[0]
+          }
         }
-      }
-    })
-    .catch(e => {
-      error(e)
-    })
+      })
+      .catch(e => {
+        error(e)
+      })
   },
-  head () {
+  head() {
     return {
+      __dangerouslyDisableSanitizersByTagID: {
+        'ip:image': ['content'],
+        'og:image': ['content'],
+        'twitter:image': ['content']
+      },
       title: this.post.fields.title,
       meta: [
-        { hid: 'description', name: 'description', content: this.post.fields.description },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.post.fields.description
+        },
         { hid: 'ip:name', itemprop: 'name', content: this.post.fields.title },
-        { hid: 'ip:headline', itemprop: 'headline', content: this.post.fields.title },
-        { hid: 'ip:description', itemprop: 'description', content: this.post.fields.description },
-        { hid: 'ip:image', itemprop: 'image', content: `https:${this.post.fields.heroImage.fields.file.url}?fit=pad&f=top&w=1200&h=630&bg=rgb:F3F6F9` },
-        { itemprop: 'datePublished', content: `${FormatDate(new Date(this.post.fields.publishDate))}` },
+        {
+          hid: 'ip:headline',
+          itemprop: 'headline',
+          content: this.post.fields.title
+        },
+        {
+          hid: 'ip:description',
+          itemprop: 'description',
+          content: this.post.fields.description
+        },
+        {
+          hid: 'ip:image',
+          itemprop: 'image',
+          content: `https:${this.post.fields.heroImage.fields.file.url}?fit=pad&f=top&w=1200&h=630&bg=rgb:F3F6F9`
+        },
+        {
+          itemprop: 'datePublished',
+          content: `${FormatDate(new Date(this.post.fields.publishDate))}`
+        },
         { hid: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:title', property: 'og:title', content: this.post.fields.title },
-        { hid: 'og:url', property: 'og:url', content: `${process.env.baseUrl}${this.$route.fullPath}` },
-        { hid: 'og:image', property: 'og:image', content: `https:${this.post.fields.heroImage.fields.file.url}?fit=pad&f=top&w=1200&h=630&bg=rgb:F3F6F9` },
-        { hid: 'og:description', property: 'og:description', content: this.post.fields.description },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.post.fields.title
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `${process.env.baseUrl}${this.$route.fullPath}`
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `https:${this.post.fields.heroImage.fields.file.url}?fit=pad&f=top&w=1200&h=630&bg=rgb:F3F6F9`
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.post.fields.description
+        },
         { hid: 'og:locale', property: 'og:locale', content: 'default' },
-        { hid: 'twitter:title', name: 'twitter:title', content: this.post.fields.title },
-        { hid: 'twitter:description', name: 'twitter:description', content: this.post.fields.description },
-        { hid: 'twitter:creator', name: 'twitter:creator', content: '@equinusocio' }
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.post.fields.title
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.post.fields.description
+        },
+        {
+          hid: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@equinusocio'
+        }
       ],
-      link: [
-        { rel: 'stylesheet', href: '/prism-theme.css' }
-      ],
+      link: [{ rel: 'stylesheet', href: '/prism-theme.css' }],
       script: [
-        { type: 'text/javascript', src: '//cdn.embedly.com/widgets/platform.js', charset: 'UTF-8', async: true, body: true }
+        {
+          type: 'text/javascript',
+          src: '//cdn.embedly.com/widgets/platform.js',
+          charset: 'UTF-8',
+          async: true,
+          body: true
+        }
       ]
     }
   },
@@ -96,7 +156,7 @@ export default {
     StoryContainer,
     VueMarkdown
   },
-  mounted () {
+  mounted() {
     Prism.highlightAll()
   },
   computed: {
