88e685f0f4468a933f0a52f20189f4b6e1fd8333
Mattia Astorino <astorino.mattia@gmail.com>
2019-07-30 10:00:57 +0200

    Hide feed icon

--- components/theme-switcher.vue
+++ components/theme-switcher.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ThemeSwitcher" role="radiogroup">
-    <a href="http://feeds.feedburner.com/equinusocio-blog" class="Icon" target="_blank">
+    <!--<a href="http://feeds.feedburner.com/equinusocio-blog" class="Icon" target="_blank">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="18"
@@ -16,7 +16,7 @@
         <path d="M4 4a16 16 0 0 1 16 16" />
         <circle cx="5" cy="19" r="1" />
       </svg>
-    </a>
+    </a>-->
     <svg
       v-on:click="setTheme(LightTheme)"
       v-on:keydown.enter="setTheme(LightTheme)"
